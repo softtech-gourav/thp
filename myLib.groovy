932feba303975633adc2b36f0d299b5758283643
Gourav patel <softtech.gourav@gmail.com>
2025-04-09 12:12:55 +0530

    Update myLib.groovy

--- myLib.groovy
+++ myLib.groovy
@@ -1,19 +1,5 @@
-// vars/myLib.groovy
-def call() {
-    // Access environment variables
-    def dockerRegistry = env.DOCKER_REGISTRY
-    def dockerRepo = env.DOCKER_REPO
-    def imageName = "${dockerRegistry}/${dockerRepo}:latest"
-    def credentialsId = env.HARBOR_CREDENTIALS_ID
-
-    // Ensure all required environment variables are set
-    if (!dockerRegistry || !dockerRepo || !credentialsId) {
-        error "Required environment variables (DOCKER_REGISTRY, DOCKER_REPO, HARBOR_CREDENTIALS_ID) are not set."
-    }
-
-    // Perform Docker pull within the registry context
-    docker.withRegistry("http://${dockerRegistry}", credentialsId) {
-        def appImage = docker.image(imageName)
-        appImage.pull()
-    }
+// vars/gitCloneProject.groovy
+def call(String projectName, String branch = 'main') {
+    def gitUtils = new org.groovenexus.GitUtils(steps)
+    gitUtils.cloneFromGit(projectName, branch)
 }
