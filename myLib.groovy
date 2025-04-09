96297ba1c772fc2c93988ce634c67a837a693876
Gourav patel <softtech.gourav@gmail.com>
2025-04-09 15:12:30 +0530

    Update myLib.groovy

--- myLib.groovy
+++ myLib.groovy
@@ -1,5 +1,23 @@
 // vars/gitCloneProject.groovy
-def call(String projectName, String branch = 'main') {
-    def gitUtils = new org.groovenexus.GitUtils(steps)
-    gitUtils.cloneFromGit(projectName, branch)
+def call(Map config = [:]) {
+    def defaults = [
+        projectName: '',
+        branch: 'main',
+        credentialsId: 'github',
+        baseUrl: 'https://bitbucket.org/thppython/',
+        useCheckout: false
+    ]
+    
+    def cfg = defaults + config
+    
+    if (!cfg.projectName) {
+        error("Project name must be specified")
+    }
+    
+    def gitUtils = new org.groovenexus.GitUtils(steps, [baseUrl: cfg.baseUrl])
+    gitUtils.cloneFromGit(cfg.projectName, [
+        branch: cfg.branch,
+        credentialsId: cfg.credentialsId,
+        useCheckout: cfg.useCheckout
+    ])
 }
