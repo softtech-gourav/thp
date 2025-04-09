bf89a6a451cc3121802f5df417869aa2ac466660
Gourav patel <softtech.gourav@gmail.com>
2025-04-09 12:12:12 +0530

    Update GitUtils.groovy

--- src/org/groovenexus/GitUtils.groovy
+++ src/org/groovenexus/GitUtils.groovy
@@ -1,15 +1,16 @@
-//src/org/groovenexus/GitUtils.groovy
-
+// src/org/groovenexus/GitUtils.groovy
 package org.groovenexus
 
 class GitUtils implements Serializable {
     def steps
+    String bitbucketBaseUrl = "https://digvijaynath@bitbucket.org/thppython/"
 
     GitUtils(steps) {
         this.steps = steps
     }
 
-    def cloneFromGit(String repoUrl, String branch = 'main') {
-        steps.git branch: branch, credentialsId: 'github', url: repoUrl
+    def cloneFromGit(String projectName, String branch = 'main') {
+        String repoUrl = "${bitbucketBaseUrl}${projectName}.git"
+        steps.git branch: branch, credentialsId: 'bitbucket-credentials', url: repoUrl
     }
 }
