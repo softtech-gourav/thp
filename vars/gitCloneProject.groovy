5d98d64628b433ea84752889ae483daf4def4c22
Gourav patel <softtech.gourav@gmail.com>
2025-04-10 12:01:48 +0530

    Update gitCloneProject.groovy

--- vars/gitCloneProject.groovy
+++ vars/gitCloneProject.groovy
@@ -1,7 +1,5 @@
-import org.groovenexus.GitUtils
-
-def call(String repoName, String branch = 'main') {
-    def gitUtils = new GitUtils(this)
-    def repoUrl = "https://github.com/softtech-gourav/${repoName}.git"
-    gitUtils.cloneFromGit(repoUrl, branch)
+// vars/gitCloneProject.groovy
+def call(String projectName, String branch = 'main') {
+    def gitUtils = new org.groovenexus.GitUtils(steps)
+    gitUtils.cloneFromGit(projectName, branch)
 }
