ba41429d3abd0786943778a5882d0babc4ea9e00
Gourav patel <softtech.gourav@gmail.com>
2025-04-18 10:51:51 +0530

    Delete myLib.groovy

--- myLib.groovy
+++
@@ -1,8 +0,0 @@
-def call(String projectName, String branch = 'main') {
-    def gitUtils = new org.groovenexus.GitUtils(steps)
-    try {
-        gitUtils.cloneFromGit(projectName, branch)
-    } catch (Exception e) {
-        steps.error "Git Clone failed: ${e.message}"
-    }
-}
