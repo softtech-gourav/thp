9e37edd9cc84dc9616175ac4597c97040ca78171
Gourav patel <softtech.gourav@gmail.com>
2025-05-02 21:45:05 +0530

    Update gitCloneProject.groovy

--- vars/gitCloneProject.groovy
+++ vars/gitCloneProject.groovy
@@ -1,5 +1,24 @@
-// vars/gitCloneProject.groovy
-def call(String projectName, String branch = 'main') {
-    def gitUtils = new org.groovenexus.GitUtils(steps)
-    gitUtils.cloneFromGit(projectName, branch)
+def cloneFromGit(String projectName, String branch = 'main') {
+    if (!projectName) {
+        throw new IllegalArgumentException("Project name cannot be empty")
+    }
+
+    String repoUrl = "${bitbucketBaseUrl}${projectName}.git"
+    steps.echo "Cloning from: ${repoUrl}"
+
+    try {
+        steps.checkout([
+            $class: 'GitSCM',
+            branches: [[name: "*/${branch}"]],
+            userRemoteConfigs: [[
+                url: repoUrl,
+                credentialsId: 'bitbucket-credentials'
+            ]],
+            extensions: [
+                [$class: 'CloneOption', noTags: false, shallow: false]
+            ]
+        ])
+    } catch (Exception e) {
+        steps.error "Failed to clone repository: ${e.message}"
+    }
 }
