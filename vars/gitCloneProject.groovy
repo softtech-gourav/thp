6c37c4a3e68fbf8c63907f40a0e64664c55fa22d
Gourav patel <softtech.gourav@gmail.com>
2025-05-03 09:25:41 +0530

    Update gitCloneProject.groovy

--- vars/gitCloneProject.groovy
+++ vars/gitCloneProject.groovy
@@ -1,5 +1,14 @@
-// vars/gitCloneProject.groovy
-def call(String projectName, String branch = 'main') {
-    def gitUtils = new org.groovenexus.GitUtils(steps)
-    gitUtils.cloneFromGit(projectName, branch)
+def call() {
+    sh "git fetch --tags"
+
+    def latestCommit = sh(script: "git rev-list --tags --max-count=1", returnStdout: true).trim()
+
+    if (!latestCommit) {
+        echo "⚠️ No tags found. Using fallback version."
+        return "v0.0.1"
+    }
+
+    def tag = sh(script: "git describe --tags ${latestCommit}", returnStdout: true).trim()
+    echo "✅ Latest Git tag is: ${tag}"
+    return tag
 }
